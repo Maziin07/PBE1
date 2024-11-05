8dd05a027fe71716239881a02ada0bb2c6a52759
Matheus Alves <159180480+Maziin07@users.noreply.github.com>
2024-11-05 07:56:51 -0300

    Delete ATV-10-MA/AT10-apiBiblioteca directory

--- ATV-10-MA/AT10-apiBiblioteca/src/main/java/br/com/bibiotecasenai/repositories/EditoraRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.bibiotecasenai.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.bibiotecasenai.controllers.EditoraController;
-
-public interface EditoraRepository extends JpaRepository<EditoraController, Long>{
-
-}
--- ATV-10-MA/AT10-apiBiblioteca/src/main/java/br/com/bibiotecasenai/repositories/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.bibiotecasenai.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.bibiotecasenai.controllers.LivroController;
-
-public interface LivroRepository extends JpaRepository<LivroController, Long>{
-
-}
